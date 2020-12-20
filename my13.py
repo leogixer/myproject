0e04bcf2e7a51c6e414683cfdedcbe7ec53a9a34
leogixer <76402058+leogixer@users.noreply.github.com>
2020-12-20 20:30:39 +0300

    Create my13.py

---
+++ my13.py
@@ -0,0 +1,9 @@
+A = [int(i) for i in input('Введите массив: ').split()]
+print(A)
+zn = max(A)
+count = 0
+for j in range(len(A)):
+    if A[j] == max(A):
+        count += 1
+print('Максимальное значение: ', max(A))
+print('Количество элементов', count)
